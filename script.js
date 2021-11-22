b54daed8d0204cd803b8988fefa0348b783e30a2
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-11-22 10:10:21 +0530

    fetching random meal

--- script.js
+++ script.js
@@ -49,11 +49,25 @@ function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            //console.log(data)
             const meal = data.meals[0];
 
             addMealToDOM(meal);
         });
+}
+
+//Fetch random meal
+function getRandomMeal() {
+    //Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+        })
 }
 
 //Add meal to DOM
@@ -89,6 +103,7 @@ function addMealToDOM(meal) {
 
 //Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
